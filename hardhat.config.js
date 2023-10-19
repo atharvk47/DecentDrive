9e60d7431e052905e5cdf389b5f9741f045ab5a9
ThisisAtharv <atharv.kurlapkar47@gmail.com>
2023-10-19 23:37:12 +0530

    Hardhat config file updated.

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+  paths: {
+    artifacts : "./client/src/artifacts",  // Creation of artifact file on deploying smart contract inside src
+  },
 };
